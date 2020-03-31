fc989eba994b5101d0c96d3edf2ae0f3f7d07b6a
sharco-code <xsharco8@gmail.com>
2020-03-31 16:18:39 +0200

    nuevo-correo commit 1

---
+++ src/app/Components/nuevo-correo/nuevo-correo.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-nuevo-correo',
+  templateUrl: './nuevo-correo.component.html',
+  styleUrls: ['./nuevo-correo.component.scss']
+})
+export class NuevoCorreoComponent implements OnInit {
+
+  nuevoCorreo: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.nuevoCorreo = this.formBuilder.group({
+          titulo: ['', [Validators.required, Validators.minLength(3)]],
+          cuerpo: ['', [Validators.required, Validators.minLength(10)]],
+          destinatario: ['', [Validators.required, Validators.email]],
+        });
+    }
+
+    get formulario() { return this.nuevoCorreo.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.nuevoCorreo.invalid) {
+            return;
+        }
+
+        let correo = this.nuevoCorreo.value;
+        correo.leido= false;
+        correo.emisor= 'correoEmisor1@openWebinar.inv';
+
+        alert("Correo Enviado \nEliminamos el formulario");
+        this.onReset();
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.nuevoCorreo.reset();
+    }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
 import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
-import { FormsModule } from '@angular/forms';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
     ListaCorreosComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
