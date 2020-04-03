fa965cf99f87cc7eaafb7abb9d27e75f0ca2aab3
sharco-code <xsharco8@gmail.com>
2020-04-03 21:17:46 +0200

    conexion componentes con servicios

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('CursoOpenWebinars app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to cursoIniciacionAngular8!');
   });
 
   afterEach(async () => {
--- src/app/Components/lista-correos/lista-correos.component.ts
+++ src/app/Components/lista-correos/lista-correos.component.ts
@@ -1,79 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { GmailService } from 'src/app/Services/gmail.service';
-import { Router } from '@angular/router';
-import { trigger, state, transition, style, animate } from '@angular/animations';
-import { MatTableDataSource } from '@angular/material/table';
-import { AvisosService } from 'src/app/Services/avisos.service';
 
 @Component({
   selector: 'app-lista-correos',
   templateUrl: './lista-correos.component.html',
-  styleUrls: ['./lista-correos.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+  styleUrls: ['./lista-correos.component.scss']
 })
 export class ListaCorreosComponent implements OnInit {
 
   correos: any[];
-  columnsToDisplay: string[] = ['Emisor', 'Asunto', 'Acciones'];
-  displayedColumns: string[] = ['emisor', 'titulo', 'id'];
-  dataSource = new MatTableDataSource<any>();
-  expandedElement: any | null;
+  responder: boolean;
+  correoAResponder: any;
 
-  constructor(private gmail: GmailService, private router: Router, private servicioAvisos: AvisosService) {
+  constructor() {
+    const correo1 = {
+      titulo: "Titulo del 1",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email`,
+      emisor: 'correoEmisor1@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: true,
+    };
+    const correo2 = {
+      titulo: "Titulo del 2",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor2@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    };
     this.correos = [];
+    this.correos.push(correo1);
+    this.correos.push(correo2);
+
+    this.correos.push({
+      titulo: "Titulo del 3",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor3@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+    this.correos.push({
+      titulo: "Titulo del 4",
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor4@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+
+    this.responder = false;
   }
 
   ngOnInit() {
-    this.getRecibidos();
   }
 
-  accionRespuestaRapida() {
-    this.expandedElement = null;
+  clickResponder(correo) {
+    correo.responder = !correo.responder;
   }
 
-  getRecibidos() {
-    this.gmail.getRecibidos().subscribe(
-      (response) => {
-        const mensajes = response.messages;
-        
-        mensajes.forEach(element => {
-          this.getMensaje(element.id);
-        });
-      },
-      (error) => this.error(error)
-    );
+  accionRespuestaRapida(correo) {
+    correo.responder = false;
   }
 
-  getMensaje(id: string){
-    this.gmail.getMessage(id).subscribe(
-      (response) => {
-        const emisor = response.payload.headers.find(e => e.name === "From");
-        const subject = response.payload.headers.find(e => e.name === "Subject");
-
-        const mensage = {
-          id: response.id,
-          cuerpo: response.snippet,
-          emisor: emisor? emisor.value : undefined,
-          titulo: subject? subject.value : undefined,
-        };
-        this.dataSource.data.push(mensage);
-        this.dataSource._updateChangeSubscription();
-      },
-      (error) => this.error(error)
-    );
-  }
-
-  error(error){
-    this.servicioAvisos.showMenssage("Se ha producido un error", 'Error');
-  }
-
-  verDetalle(correo){
-    this.router.navigate(['/mail', {correo: JSON.stringify(correo)}]);
-  }
 }
